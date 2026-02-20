8a022695dffbcb2bcd040358ff61e153a32255d5
Kamui <kamui@Kamui.localdomain>
2026-02-20 12:07:50 -0500

    lsc initial bins

--- serial.cpp
+++ serial.cpp
@@ -1,5 +1,8 @@
 #include "common.h"
 #include <cmath>
+#include <vector>
+#include <iostream>
+#include <utility>
 
 // Apply the force from neighbor to particle
 void apply_force(particle_t& particle, particle_t& neighbor) {
@@ -42,24 +45,106 @@ void move(particle_t& p, double size) {
     }
 }
 
+const float bin_size = 0.03;
+static std::vector<std::pair<int, std::vector<int>>> bins;
+static int num_bins;
 
 void init_simulation(particle_t* parts, int num_parts, double size) {
 	// You can use this space to initialize static, global data objects
     // that you may need. This function will be called once before the
     // algorithm begins. Do not do any particle simulation here
+    int bin_div = (int) (size/bin_size);
+    num_bins = (bin_div == size/bin_size) ? bin_div : bin_div + 1;
+
+    //std::cout << "num bins: " << num_bins << " size: " << size << std::endl;
+
+    bins = std::vector<std::pair<int,std::vector<int>>>(num_bins*num_bins, std::pair<int,std::vector<int>>(0, std::vector<int>()));
+
+    
 }
+
+inline void compute_inter_bin(particle_t* parts, int bin_i1, int bin_i2){
+    //std::cout << "(" << bin_i1 % num_bins << "," << bin_i1 / num_bins << ") <-> (" 
+    //<< bin_i2 % num_bins << "," << bin_i2 / num_bins << ")\n";
+
+    for(int i = 0; i < std::get<0>(bins[bin_i1]); i++){
+        int curr_part =  std::get<1>(bins[bin_i1])[i];
+        for(int j = 0; j < std::get<0>(bins[bin_i2]); j++){
+            apply_force(parts[curr_part], parts[(std::get<1>(bins[bin_i2])[j])]);
+        }
+    }
+}
+
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
     // Compute Forces
+    /*
     for (int i = 0; i < num_parts; ++i) {
         parts[i].ax = parts[i].ay = 0;
         for (int j = 0; j < num_parts; ++j) {
             apply_force(parts[i], parts[j]);
         }
     }
+    
+    */
+   for(int i = 0; i < num_parts; i++){
+        //std::cout << "P: " << parts[i].x << "," << parts[i].y << " to bin " << (int)(parts[i].x / bin_size) << ","
+        //<< (int)(parts[i].y / bin_size) << std::endl;
+        parts[i].ax = parts[i].ay = 0;
+        int curr_idx = (int)(parts[i].x / bin_size) + ((int)(parts[i].y / bin_size))*num_bins;
+        if(++(std::get<0>(bins[curr_idx])) > std::get<1>(bins[curr_idx]).size()){
+            std::get<1>(bins[curr_idx]).push_back(i);
+        } else{
+           std::get<1>(bins[curr_idx])[std::get<0>(bins[curr_idx])-1] = i;
+        }
 
+    }
+    
+    for(int i = 0; i < num_bins*num_bins; i++){
+        bool x_bound = i % num_bins == num_bins - 1; 
+        bool y_bound = i / num_bins == num_bins - 1;
+        bool x_bound_top = i % num_bins == 0;
+        bool y_bound_top = i / num_bins == 0;
+
+        int y_next = i + num_bins;
+        int y_prev = i - num_bins;
+        
+        if(!x_bound && !y_bound_top){
+            compute_inter_bin(parts, i, y_prev + 1);
+        }
+        if(!x_bound && !y_bound){
+            compute_inter_bin(parts, i, y_next + 1);
+        }
+        if(!x_bound){
+            compute_inter_bin(parts, i, i + 1);
+        }
+        if(!x_bound_top){
+            compute_inter_bin(parts, i, i - 1);
+        }
+
+        if(!x_bound_top && !y_bound_top){
+            compute_inter_bin(parts, i, y_prev - 1);
+        }
+        if(!x_bound_top && !y_bound){
+            compute_inter_bin(parts, i, y_next - 1);
+        }
+        if(!y_bound){
+            compute_inter_bin(parts, i, y_next);
+        }
+        if(!y_bound_top){
+            compute_inter_bin(parts, i, y_prev);
+        }
+
+        compute_inter_bin(parts, i, i);
+    }
+    
+
+    for(int i = 0; i < num_bins*num_bins; i++){
+        std::get<0>(bins[i]) = 0;
+    }
     // Move Particles
     for (int i = 0; i < num_parts; ++i) {
         move(parts[i], size);
     }
+    //std::cout << "\n\n";
 }
